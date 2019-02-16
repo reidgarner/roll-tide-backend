14bf9e41180a7bb6ce6af2502fd953d5a8d89ea6
reidgarner <reidbgarner@gmail.com>
2019-02-16 16:50:08 -0700

    made beach_routes.js

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express()
 const bodyParser = require ('body-parser')
 const cors = require('cors')
 const port = process.env.PORT || 3003
+const beaches = require('./routes/beaches_routes')
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+app.use('/beaches', beaches)
 
 app.listen (port, () => {
     console.log(`running on port: ${port}`)
