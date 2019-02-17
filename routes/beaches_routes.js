1a782588c259be04e689a01462f5f6cc9fd60df0
reidgarner <reidbgarner@gmail.com>
2019-02-16 18:14:11 -0700

    creates beaches routes

--- routes/beaches_routes.js
+++ routes/beaches_routes.js
@@ -6,6 +6,21 @@ router.get('/', (req,res) => {
     queries.readAllBeaches().then(beaches => res.status(200).send({beaches}))
 })
 
+router.get('/:id', (req,res) => {
+    if(!parseInt(req.params.id))res.send('not valid integer')
+    queries.readBeachById(req.params.id).then(beach => res.status(200).send({ beach }))
+})
 
+router.post('/',(req,res) => {
+    queries.createBeach(req.body).then(newBeach => res.status(201).send(newBeach))
+})
+
+router.put('/:id',(req,res) => {
+    queries.updateBeach(req.params.id,req.body).then(updatedBeach => res.status(200).send(updatedBeach))
+})
+
+router.delete('/:id',(req,res) => {
+    queries.deleteBeach(req.params.id).then(() => res.sendStatus(204))
+})
 
 module.exports = router
